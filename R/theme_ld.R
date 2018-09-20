36c9eb36052de0bf28a2cdaa16dd1ffdccbf242c
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-09-20 15:44:30 +0200

    start working on theme

---
+++ R/theme_ld.R
@@ -0,0 +1,24 @@
+#' Locke Data ggplot2 theme
+#'
+#' @return ggplot2 theme
+#' @export
+#'
+#' @examples
+#' library("ggplot2")
+#' df <- data.frame(
+#' gp = factor(rep(letters[1:3], each = 10)),
+#' y = rnorm(30)
+#' )
+#' ggplot(df, aes(gp, y)) +
+#'   geom_point() +
+#'   theme_ld()
+
+theme_ld <- function(){
+  ld <- ggplot2::theme_minimal(base_family = "Roboto-Bold",
+                               base_size = 12)
+
+  ld <- ld + theme(legend.background=element_blank())
+  ld <- ld + theme(legend.key=element_blank())
+
+  return(ld)
+}
