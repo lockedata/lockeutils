ee3d1943ff3469c861be5b61f750636309bf8c66
Stephanie Locke <stephanie.g.locke@gmail.com>
2018-09-24 16:01:53 +0100

    Added grey back in

--- R/ld_colours.R
+++ R/ld_colours.R
@@ -8,9 +8,11 @@
 get_ld_colours <- function(){
   tibble::tibble(colour_name = c("blue", "orange",
                                  "teal", "red",
-                                 "green", "pink"),
+                                 "green", "pink",
+                                 "grey"),
                  colour_hex = c("#2165b6", "#e8830c",
                                 "#3cb4b8", "#cf4729",
-                                "#34ba17", "#c04aa9")
+                                "#34ba17", "#c04aa9",
+                                "#4d4d4d")
                 )
 }
