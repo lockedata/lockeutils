979b502133aea392f84c6dee51e062fb43482af8
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-09-24 12:38:32 +0200

    add colours

---
+++ R/ld_colours.R
@@ -0,0 +1,13 @@
+' Locke Data official colours
+#'
+#' @return A tibble with colours' names and hex codes.
+#' @export
+#'
+#' @examples
+#' get_ld_colours()
+get_ld_colours <- function(){
+  tibble::tibble(colour_name = c("orange", "blue",
+                                 "grey"),
+                 colour_hex = c("#E8830C", "#2165B6",
+                                "#4d4d4d"))
+}
