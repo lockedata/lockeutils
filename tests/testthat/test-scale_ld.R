90382a327f246b60d0a563ee46d043c4ad28d786
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-09-24 18:32:33 +0200

    fix test

--- tests/testthat/test-scale_ld.R
+++ tests/testthat/test-scale_ld.R
@@ -4,7 +4,8 @@ test_that("scale_fill_ld works", {
   fill <- scale_fill_ld()
   testthat::expect_is(fill, "ScaleDiscrete")
   testthat::expect_true(all(fill$palette(3) ==
-                              c("#E8830C", "#2165B6", "#4d4d4d")))
+                              c("#2165b6", "#e8830c", "#3cb4b8", "#cf4729", "#34ba17", "#c04aa9",
+                                "#4d4d4d")))
   testthat::expect_equal(fill$aesthetics, "fill")
 })
 
@@ -12,6 +13,7 @@ test_that("scale_colour_ld works", {
   colour <- scale_colour_ld()
   testthat::expect_is(colour, "ScaleDiscrete")
   testthat::expect_true(all(colour$palette(3) ==
-                              c("#E8830C", "#2165B6", "#4d4d4d")))
+                              c("#2165b6", "#e8830c", "#3cb4b8", "#cf4729", "#34ba17", "#c04aa9",
+                                "#4d4d4d")))
   testthat::expect_equal(colour$aesthetics, "colour")
 })
