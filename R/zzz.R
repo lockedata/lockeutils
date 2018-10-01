b357706febf09e2acf6c3d6582ac6c17323b1ed4
Dave Parr <DavidRGParr@gmail.com>
2018-10-01 21:55:02 +0100

    include check for empty table return

--- R/zzz.R
+++ R/zzz.R
@@ -8,16 +8,24 @@
   }
 
   fnt <- extrafont::fonttable()
-  if (!any(grepl("Contrail|Roboto", fnt$FamilyName))) {
-    packageStartupMessage("NOTE: Contrail One or Roboto fonts are required to use this package.")
-    packageStartupMessage("      Please use lockeutils::import_roboto() to install Roboto and")
-    packageStartupMessage("      lockeutils::import_contrail() to install Contrail One")
-    if(interactive()) {
-      switch(menu(c("Yes", "No"), title = "Do you want to index fonts using extrafont::font_import()?") + 1,
-             cat("nothing Done\n"),
-             extrafont::font_import(),
-             "Not indexing")
-    }
-  } # nocov end
+  if(!"Contrail One" %in% fnt$FamilyName){
+    packageStartupMessage("NOTE: Contrail One is required to use this package.")
+    packageStartupMessage("      Please use lockeutils::import_contrail()")
+  }
 
+  if(!"Roboto Thin" %in% fnt$FamilyName){
+    packageStartupMessage("NOTE: Roboto Thin is required to use this package.")
+    packageStartupMessage("      Please use lockeutils::import_roboto()")
+  }
+
+  if(nrow(fnt) == 0 & interactive()) {
+    switch(
+      menu(c("Yes", "No"),
+           title = "Do you want to index fonts using extrafont::font_import()?") + 1,
+      cat("nothing Done\n"),
+      extrafont::font_import(),
+      "Not indexing"
+    )
+  }
 }
+
